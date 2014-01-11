baf4af8299421117d3e8198cebc96133f0f684f3
Evan Rosson <github@erosson.org>
2014-01-11 00:54:51 -0800

    Pause/resume button in game. Fixes #7. GUI usage example might help with #13.

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	public Texture2D pauseIcon;
+	public Texture2D unpauseIcon;
+	public AudioClip pauseSound;
+	public AudioClip unpauseSound;
+	private bool paused = false;
+
+	// http://docs.unity3d.com/Documentation/Components/GUIScriptingGuide.html
+	void OnGUI() {
+		// http://answers.unity3d.com/questions/46158/how-to-create-a-transparent-button.html
+		GUI.backgroundColor = Color.clear;
+		Rect buttonRect = new Rect(0, 0, 50, 50);
+		if (paused) {
+			paused = !GUI.Button(buttonRect, unpauseIcon);
+			if (!paused) {
+				audio.PlayOneShot(unpauseSound);
+				Time.timeScale = 1;
+			}
+		}
+		else {
+			paused = GUI.Button(buttonRect, pauseIcon);
+			if (paused) {
+				audio.PlayOneShot(pauseSound);
+				// http://answers.unity3d.com/questions/7544/how-do-i-pause-my-game.html
+				// Prevents fruits from falling, and also seems to prevent matching on click.
+				Time.timeScale = 0;
+			}
+		}
+	}
+}
